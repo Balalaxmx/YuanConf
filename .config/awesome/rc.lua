7cc6c4d59e92ad9fbf3f0b4f14581f89135ad159
YuanPre <balalaxmxtlm@icloud.com>
2017-04-09 12:08:16 +0800

    Using vertical fair window mode to default awesome mode

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -60,7 +60,6 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-   awful.layout.suit.floating,
    awful.layout.suit.fair,
    awful.layout.suit.fair.horizontal,
    awful.layout.suit.max,
@@ -68,6 +67,7 @@ awful.layout.layouts = {
    awful.layout.suit.corner.nw,
    awful.layout.suit.tile,
    awful.layout.suit.magnifier,
+   awful.layout.suit.floating,
    -- awful.layout.suit.tile.left,
    -- awful.layout.suit.tile.top,
    -- awful.layout.suit.spiral.dwindle,
