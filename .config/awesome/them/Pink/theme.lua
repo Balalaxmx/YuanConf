7f1a5ea89fb6d334788a788d26a4280c9a79767e
YuanPre <balalaxmxtlm@icloud.com>
2017-04-16 15:32:49 +0800

    Fixed fair bugs in Awesome WM theme.

--- .config/awesome/them/Pink/theme.lua
+++ .config/awesome/them/Pink/theme.lua
@@ -59,7 +59,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = themes_path.."Pink/submenu.png"
 theme.menu_height = dpi(16)
 theme.menu_width  = dpi(128)
 
